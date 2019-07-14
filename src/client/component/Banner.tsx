63a10f978f7ba5eebc7982ae7ab8cc87ffb12898
Kyle Phillips <kylep@Kyles-imac.fios-router.home>
2019-07-14 16:17:25 -0400

    Changed color of Banner

--- src/client/component/Banner.tsx
+++ src/client/component/Banner.tsx
@@ -5,8 +5,8 @@ import React from 'react';
 const Banner = () => {
     return (
         <div className="container" style={{ position: 'relative' }}>
-            <div className="jumbotron">
-                <h1 style={{ fontSize: '70px' }}>W E A T H E R N O W</h1>
+            <div className="jumbotron ">
+                <h1 style={{ fontSize: '70px',color:'#CFB5B5' }}>W E A T H E R N O W</h1>
             </div>
         </div>
     )
