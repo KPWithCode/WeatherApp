feea03b8378704f3b9f4bd40ff4e8b2dcc6fd390
Kyle Phillips <kylep@Kyles-imac.fios-router.home>
2019-07-14 19:28:23 -0400

    Minor Changes

--- src/client/component/Form.tsx
+++ src/client/component/Form.tsx
@@ -11,19 +11,22 @@ const Form = () => {
 
 
   return (
-    <div className="border border-warning rounded d-flex justify-content-center">
-        <label
-        style={{ fontSize: '35px', color: '#CFB5B5',marginLeft:'39%' }}>E N T E R    Z I P</label>
-   
-      <form style={{ position: 'relative', width: '100%' }}>
-        <div className="form-group d-flex justify-content-center" style={{ marginTop: '20%', marginLeft: '35%' }}>
+    <div>
+      <label
+        style={{ fontSize: '35px', color: '#CFB5B5', }}>
+          E N T E R    Z I P</label>
+      {/* marginLeft:'39%' */}
 
-          <input className="d-flex justify-content-center  form-control " min={5} max={5} style={{ width: '40%', height: '5%' }} type="number" placeholder="Enter Zip Code"></input>
-          
-          <button style={{ height: '5%', width: '10%' }} type="submit" className="btn btn-primary">Submit</button>
-          <br />
-          <small style={{color:'#CFB5B5'}} id="emailHelp" className="form-text text-muted">TRY IT OUT.</small>
-        </div>
+      <form style={{ position: 'relative' }}>
+        {/* <div className="form-group d-flex justify-content-center" style={{ marginTop: '20%', marginLeft: '35%' }}> */}
+
+        <input className="d-flex justify-content-center  form-control " min={5} max={5}
+          style={{ width: '40%', height: '5%' }} type="number" placeholder="Enter Zip Code"></input>
+
+        <button style={{ height: '5%', width: '10%' }} type="submit" className="btn btn-primary">Submit</button>
+        <br />
+        <small style={{ color: '#CFB5B5' }} id="emailHelp" className="form-text text-muted">TRY IT OUT.</small>
+        {/* </div> */}
       </form>
 
     </div>
--- src/client/pages/Weather.tsx
+++ src/client/pages/Weather.tsx
@@ -6,18 +6,19 @@ import Banner from '../component/Banner'
 const Weather = () => {
 
     return (
-            <div style={{ width: '100%', height: '100%', backgroundColor: '#b67b7b' }}>
-                {/* <img src="https://images.unsplash.com/photo-1556706995-b5b014972b9c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2523&q=80"
+        <div className=""
+            style={{ width: '100%', height: '100%', backgroundColor: '#b67b7b' }}>
+            {/* <img src="https://images.unsplash.com/photo-1556706995-b5b014972b9c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2523&q=80"
                 alt="weather"
                 style={{ width: '20%', height: '12%', position: 'absolute' }}
             /> */}
-                <div>
-                    <Banner />
-                </div>
-                <div className="">
-                    <Form />
-                </div>
+            <div>
+                <Banner />
             </div>
+            <div className="d-flex justify-content-center">
+                <Form />
+            </div>
+        </div>
 
     )
 
